4ea71d5b8e224dc97ce2c6690fa14d78369db73f
Andrey <4ponomarev@gmail.com>
2025-08-22 11:09:52 +0500

    Удаление записей о прогрессе пользователя при удалении пользователя

--- app/models/clients.py
+++ app/models/clients.py
@@ -10,16 +10,22 @@ class Client(db.Model):
     password = db.Column(db.String, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
 
+    # Каскадное удаление прогресса
+    progress = db.relationship(
+        "Progress",
+        backref="client",
+        lazy=True,
+        cascade="all, delete-orphan"
+    )
+
 
 class Progress(db.Model):
     __bind_key__ = "dicts"
     __tablename__ = "progress"
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("clients.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("clients.id", ondelete="CASCADE"), nullable=False)
     word_id = db.Column(db.String, nullable=False)
     memory_score = db.Column(db.Integer, nullable=False)
     last_reviewed = db.Column(db.DateTime, nullable=False)
     swipe_up_count = db.Column(db.Integer, default=0)
     swipe_down_count = db.Column(db.Integer, default=0)
-
-    client = db.relationship("Client", backref=db.backref("progress", lazy=True))
